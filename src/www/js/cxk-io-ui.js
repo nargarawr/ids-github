c5c6bc736677dac2f70970cdddd197ce2945b148
Craig Knott <cxk01u@googlemail.com>
2014-02-17 12:05:44 +0000

    Added new IO file

---
+++ src/www/js/cxk-io-ui.js
@@ -0,0 +1,19 @@
+/*
+  cxk-io-ui.js
+  Deals with loading and saving of files to the system
+  Author: Craig Knott
+
+  Functions:
+    loadFile ( filetype );
+    saveFile ( filetype );
+*/
+
+
+function loadFile ( filetype ) {
+
+}
+
+
+function saveFile( filetype ){
+
+}
